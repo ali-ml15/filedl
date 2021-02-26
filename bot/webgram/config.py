f059b3bace0df7072a73aea81f949d1da74eaf89
ali-ml15 <79703773+ali-ml15@users.noreply.github.com>
2021-02-26 17:03:59 +0330

    Add files via upload

---
+++ bot/webgram/config.py
@@ -0,0 +1,26 @@
+import os 
+
+class Config:
+
+    class config:
+        APP_ID = 3328665
+
+        API_HASH = "281b67a17a595a09fe0a6a8afa4dd2ee"
+        
+        BOT_TOKEN = "1567115536:AAH969_15aDmlUxKFkSiKhmssk28F8leKwA"
+        
+        BOT_TOKEN2 = "1682969785:AAGXk6ACOjvAq4IlcKD7iuyuoOjSkF874D4"
+        
+        MASTER_TOKEN = ""
+        
+        channel = 'file_linkdl'
+        
+        STATS_CHANNEL = -1001240581541
+
+        ROOT_URI = f"https://dldldld.herokuapp.com"
+
+        ROOT_URI_2 = "https://dldldld.herokuapp.com"
+        
+        ROOT_URI_3 = "https://dldldld.herokuapp.com"
+
+        ENCODING = "utf8"
---
+++ web/webgram/config.py
@@ -0,0 +1,29 @@
+import os 
+
+class Config:
+
+    class config:
+        APP_ID = 3328665
+
+        API_HASH = "281b67a17a595a09fe0a6a8afa4dd2ee"
+        
+        BOT_TOKEN = "1567115536:AAH969_15aDmlUxKFkSiKhmssk28F8leKwA"
+        
+        BOT_TOKEN2 = "1682969785:AAGXk6ACOjvAq4IlcKD7iuyuoOjSkF874D4"
+        
+        STATS_CHANNEL = -1001240581541
+
+        HOST = "127.0.0.1"
+
+        PORT = os.getenv('PORT')
+
+        ROOT_URI = f"https://dldldld.herokuapp.com"
+
+        ROOT_URI_2 = "https://dldldld.herokuapp.com"
+        
+        ROOT_URI_3 = "https://dldldld.herokuapp.com"
+
+        ENCODING = "utf8"
+
+
+        # ALLOWED_EXT = ["mkv", "mp4", "flv"]
